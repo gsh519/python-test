aa2f0930629352f3186b0b850d243d73e026d555
gsh519 <gsh05196yu@yahoo.co.jp>
2021-08-24 21:49:25 +0900

    edit first question

--- search.py
+++ search.py
@@ -1,18 +1,20 @@
 
-### 検索ツールサンプル
-### これをベースに課題の内容を追記してください
-
 # 検索ソース
 source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
 
 ### 検索ツール
 def search():
-    word =input("鬼滅の登場人物の名前を入力してください >>> ")
-    
-    if word in source:
-        print("{}が見つかりした".format(word))
-    else:
-        print('{}が見つかりませんでした'.format(word))
+    while True:
+        word =input("鬼滅の登場人物の名前を入力してください >>> ")
+        
+        if word in source:
+            print("{}が見つかりした".format(word))
+        else:
+            print('{}が見つかりませんでした'.format(word))
+            source.append(word)
+            
+        with open('search.csv', 'w') as f:
+                f.write('\n'.join(source))
 
 if __name__ == "__main__":
     search()

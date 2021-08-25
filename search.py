a14156bf2c1d0b7f950de605ea579f938e7872af
gsh519 <gsh05196yu@yahoo.co.jp>
2021-08-25 20:21:33 +0900

    read csv

--- search.py
+++ search.py
@@ -1,9 +1,16 @@
-
+import os
 # 検索ソース
 source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
 
 ### 検索ツール
 def search():
+    
+    if not os.path.exists('search.csv'):
+        with open('search.csv', 'w') as f:
+            f.write('\n'.join(source))
+    with open('search.csv', 'r') as f:
+        f.read()
+    
     while True:
         word =input("鬼滅の登場人物の名前を入力してください >>> ")
         
@@ -12,9 +19,9 @@ def search():
         else:
             print('{}が見つかりませんでした'.format(word))
             source.append(word)
-            
+
         with open('search.csv', 'w') as f:
-                f.write('\n'.join(source))
+            f.write('\n'.join(source))
 
 if __name__ == "__main__":
     search()
